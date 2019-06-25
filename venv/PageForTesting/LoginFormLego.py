51ccb2d89b3f70eb1b01255c99571a735e9d3808
ludmi <ludmila.turivnenko@gmail.com>
2019-06-25 16:10:27 +0500

    created Page (using Page Object)

--- venv/PageForTesting/LoginFormLego.py
+++ venv/PageForTesting/LoginFormLego.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox(executable_path=r'C:\Users\ludmi\geckodriver')
+userlogin = driver.find_element_by_name('USER_LOGIN')
+userpass = driver.find_element_by_name('pass-oneclick')
+rememberme = driver.find_element_by_name('USER_REMEMBER')
+forgotpass = driver.find_element_by_link_text('Забыли пароль?')
+buttonlogin = driver.find_element_by_class_name('orange-bttn')
+buttonregistration = driver.find_element_by_class_name('gray-bttn')
--- venv/PageForTesting/RegistrationFormLego.py
+++ venv/PageForTesting/RegistrationFormLego.py
@@ -1,3 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Firefox(executable_path=r'C:\Users\ludmi\geckodriver')
+username = driver.find_element_by_name('NAME')
+useremail = driver.find_element_by_name('EMAIL')
+userphone = driver.find_element_by_name('PERSONAL_PHONE')
+userpassword = driver.find_element_by_name('PASSWORD')
+buttonlogin = driver.find_element_by_class_name('gray-bttn')
+buttonregistration = driver.find_element_by_class_name('orange-bttn')
